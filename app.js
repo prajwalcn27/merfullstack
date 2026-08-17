0200be79669218ff00057d2bf775f53a597fd1a9
prajwalcn27 <prajjuprajwalcn@gmail.com>
2026-08-17 09:54:42 +0530

    Remove comments from app.js routes

--- app.js
+++ app.js
@@ -3,27 +3,27 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// Home page
+
 app.get("/", (req, res) => {
     res.send("This is a Home Page");
 });
 
-// About Us page
+
 app.get("/about", (req, res) => {
     res.send("This is an About Us Page");
 });
 
-// Contact page
+
 app.get("/contact", (req, res) => {
     res.send("This is a Contact Page");
 });
 
-// Product page
+
 app.get("/product", (req, res) => {
     res.send("This is a Product Page");
 });
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
